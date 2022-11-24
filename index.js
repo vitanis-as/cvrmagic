4bef1f2db69ba76b3e9fe3d85fd608c1236ae657
Salby <srdlarsen@gmail.com>
2022-11-24 14:20:18 +0100

    Implement magic

---
+++ index.js
@@ -0,0 +1,38 @@
+class CvrMagic {
+    constructor(formSelector, inputSelector) {
+        this.inputs = {
+            cvr: document.querySelector(`${formSelector} ${inputSelector}`)
+        }
+        discoverInputs(formId)
+
+        // Listen for changes to the cvr input value.
+        this.inputs.cvr.addEventListener("change", (e) => {
+            fetchCvrData(e.value)
+                .then((data) => {
+                    fillForm(data)
+                })
+        })
+    }
+
+    fetchCvrData(cvr) {
+        return fetch(`https://cvrapi.dk/api?search=${cvr}&country=dk`)
+            .then((res) => JSON.parse(res))
+            .catch((error) => {
+                console.log(`Couldn't fetch company data from CVR: ${error}`)
+            })
+    }
+
+    discoverInputs(formSelector) {
+        this.inputs.company = document.querySelector(`${formSelector} input[name="company"]`)
+        this.inputs.address = document.querySelector(`${formSelector} input[name="address"]`)
+        this.inputs.zipCode = document.querySelector(`${formSelector} input[name="zipcode"]`)
+        this.inputs.city = document.querySelector(`${formSelector} input[name="city"]`)
+    }
+
+    fillForm(companyData) {
+        this.inputs.company.value = companyData.name
+        this.inputs.address.value = companyData.address
+        this.inputs.zipCode.value = companyData.zipcode
+        this.inputs.city.value = companyData.city
+    }
+}
